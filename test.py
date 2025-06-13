8a879121130ae1cdc00c3c42325d99cc56ad7614
Ernur <124793455+A1z3n007@users.noreply.github.com>
2025-06-13 21:36:48 +0500

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,15 @@ from tel_bot1 import bool_1, process_article
 from types import SimpleNamespace
 
 def test_start_authorized():
-    c_id = 1795671737
-    return bool_1(c_id) == True
+    message = SimpleNamespace(chat=SimpleNamespace(id=1795671737))
+    return bool_1(message) == True
 
 def test_start_unauthorized():
-    c_id = 1234567890
-    return bool_1(c_id) == False
+    message = SimpleNamespace(chat=SimpleNamespace(id=1234567890))
+    return bool_1(message) == True
 
 def test_art_find_valid():
-    message = SimpleNamespace(chat=SimpleNamespace(id=1795671737), text="148265572")
+    message = SimpleNamespace(chat=SimpleNamespace(id=1795671737), text="272181996")
     try:
         process_article(message)
         return True
@@ -24,7 +24,7 @@ def test_art_find_valid_2():
         process_article(message)
         return True
     except Exception as e:
-        print("Ошибка в test_art_find_valid:", e)
+        print("Ошибка в test_art_find_valid_2:", e)
         return False
 
 print(test_start_authorized())
